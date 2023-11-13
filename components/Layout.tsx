feecf5ce7360825b94660fb7811ee7e0dc1577ef
Lee Jackson <130007945+Imagineer99@users.noreply.github.com>
2023-11-13 19:39:44 +0000

    Update 7 files

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,13 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title = 'Chakra Template' }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
@@ -21,12 +21,42 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
         <a href="/api/users">Users API</a>
       </nav>
     </header>
-    {children}
+    <main>
+      <section className="hero">
+        <div className="hero-content">
+          <h1>{title}</h1>
+          <p>Your catchphrase or a brief description goes here.</p>
+          {/* Add any other elements like buttons or links */}
+        </div>
+      </section>
+      <section className="main-content">
+        {children}
+        {/* Add additional content specific to the landing page */}
+      </section>
+    </main>
     <footer>
       <hr />
       <span>I'm here to stay (Footer)</span>
     </footer>
-  </div>
-)
 
-export default Layout
+    <style jsx>{`
+      .hero {
+        background: url('/hero-background.jpg') center/cover;
+        color: #fff;
+        text-align: center;
+        padding: 100px 0;
+      }
+
+      .hero-content {
+        max-width: 600px;
+        margin: 0 auto;
+      }
+
+      .main-content {
+        padding: 20px;
+      }
+    `}</style>
+  </div>
+);
+
+export default Layout;
